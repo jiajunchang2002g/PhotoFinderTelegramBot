8d3b70ca3a5487b375a0c6d44e186f9d5ecb1cdb
jiajunchang2002g <jiajunchang2002@gmail.com>
2024-05-20 23:08:02 +0800

    screaming bot (ready)

--- src/main/java/tutorial/Bot.java
+++ src/main/java/tutorial/Bot.java
@@ -1,11 +1,14 @@
 package tutorial;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
+    private boolean screaming = false;
 
     @Override
     public String getBotUsername() {
@@ -17,7 +20,7 @@ public class Bot extends TelegramLongPollingBot {
         return "7164195520:AAHQMBXOMHghUmz6_6--3B4UZeduN847TcI";
     }
 
-    public void sendText(Long who, String what){
+    public void sendText(Long who, String what) {
         SendMessage sm = SendMessage.builder()
                 .chatId(who.toString()) //Who are we sending a message to
                 .text(what).build();    //Message content
@@ -34,10 +37,29 @@ public class Bot extends TelegramLongPollingBot {
         var user = msg.getFrom();
         var id = user.getId();
 
-        copyMessage(id, msg.getMessageId());
+        if (!msg.isCommand()) {
+            if (screaming)
+                scream(id, update.getMessage());
+            else
+                copyMessage(id, msg.getMessageId());
+        } else {
+            if (msg.getText().equals("/scream"))         //If the command was /scream, we switch gears
+                screaming = true;
+            else if (msg.getText().equals("/whisper"))  //Otherwise, we return to normal
+                screaming = false;
+
+            return;
+        }
     }
 
-    public void copyMessage(Long who, Integer msgId){
+    private void scream(Long id, Message msg) {
+        if (msg.hasText())
+            sendText(id, msg.getText().toUpperCase());
+        else
+            copyMessage(id, msg.getMessageId());
+    }
+
+    public void copyMessage(Long who, Integer msgId) {
         CopyMessage cm = CopyMessage.builder()
                 .fromChatId(who.toString())  //We copy from the user
                 .chatId(who.toString())      //And send it back to him
