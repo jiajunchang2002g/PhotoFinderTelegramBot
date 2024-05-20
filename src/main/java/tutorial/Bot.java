d9896ef654d57d9b74eb668d877339b7aaad223c
jiajunchang2002g <jiajunchang2002@gmail.com>
2024-05-21 00:00:19 +0800

    menu button (not ready)

--- src/main/java/tutorial/Bot.java
+++ src/main/java/tutorial/Bot.java
@@ -1,14 +1,24 @@
 package tutorial;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.List;
+
 public class Bot extends TelegramLongPollingBot {
+
     private boolean screaming = false;
+    private InlineKeyboardMarkup keyboardM1;
+    private InlineKeyboardMarkup keyboardM2;
 
     @Override
     public String getBotUsername() {
@@ -33,9 +43,30 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        var next = InlineKeyboardButton.builder()
+                .text("Next").callbackData("next")
+                .build();
+
+        var back = InlineKeyboardButton.builder()
+                .text("Back").callbackData("back")
+                .build();
+
+        var url = InlineKeyboardButton.builder()
+                .text("Tutorial")
+                .url("https://core.telegram.org/bots/api")
+                .build();
         var msg = update.getMessage();
         var user = msg.getFrom();
         var id = user.getId();
+        var text = msg.getText();
+
+        keyboardM1 = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(next)).build();
+
+        keyboardM2 = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(back))
+                .keyboardRow(List.of(url))
+                .build();
 
         if (!msg.isCommand()) {
             if (screaming)
@@ -43,12 +74,62 @@ public class Bot extends TelegramLongPollingBot {
             else
                 copyMessage(id, msg.getMessageId());
         } else {
-            if (msg.getText().equals("/scream"))         //If the command was /scream, we switch gears
-                screaming = true;
-            else if (msg.getText().equals("/whisper"))  //Otherwise, we return to normal
-                screaming = false;
+            switch (text) {
+                case "/scream" ->
+                        screaming = true;
+                case "/whisper" ->
+                        screaming = false;
+                case "/menu" -> sendMenu(id, "<b>Menu 1</b>", keyboardM1);
+            }
+        }
+    }
 
-            return;
+    public void sendMenu(Long who, String txt, InlineKeyboardMarkup kb){
+        SendMessage sm = SendMessage.builder().chatId(who.toString())
+                .parseMode("HTML").text(txt)
+                .replyMarkup(kb).build();
+
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void buttonTap(Long id, String queryId, String data, int msgId) {
+
+        EditMessageText newTxt = EditMessageText.builder()
+                .chatId(id.toString())
+                .messageId(msgId).text("").build();
+
+        EditMessageReplyMarkup newKb = EditMessageReplyMarkup.builder()
+                .chatId(id.toString()).messageId(msgId).build();
+
+        if(data.equals("next")) {
+            newTxt.setText("MENU 2");
+            newKb.setReplyMarkup(keyboardM2);
+        } else if(data.equals("back")) {
+            newTxt.setText("MENU 1");
+            newKb.setReplyMarkup(keyboardM1);
+        }
+
+        AnswerCallbackQuery close = AnswerCallbackQuery.builder()
+                .callbackQueryId(queryId).build();
+
+        try {
+            execute(close);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            execute(newTxt);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            execute(newKb);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
         }
     }
 
